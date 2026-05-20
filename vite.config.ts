df373b5c7b1463ff2641fe111c8e53c78e75292d
Yliubov <129996599+YLiubov@users.noreply.github.com>
2026-05-20 13:16:10 +0200

    fix github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite' // Importerer Vite config helper
 import tailwindcss from '@tailwindcss/vite' // Importerer Tailwind Vite plugin
 
 export default defineConfig({
+  base: "/memory-game-ts/",
   plugins: [
     tailwindcss(), // Aktiverer Tailwind i build pipeline
   ],
